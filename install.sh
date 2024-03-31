4b9527f9b190fb6fd77395fc38e618509ce597e8
ngedot <152137599+ngedot@users.noreply.github.com>
2024-03-31 11:11:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -223,19 +223,19 @@ function first_setup(){
     print_success "Directory Xray"
     if [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "ubuntu" ]]; then
     echo "Setup Dependencies $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    #sudo apt update -y
-    #apt-get install --no-install-recommends software-properties-common
-    #add-apt-repository ppa:vbernat/haproxy-2.4 -y
-    apt-get -y install haproxy #haproxy=2.4.\*
+    sudo apt update -y
+    apt-get install --no-install-recommends software-properties-common
+    add-apt-repository ppa:vbernat/haproxy-2.4 -y
+    apt-get -y install haproxy=2.4.\*
 elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
     echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    #curl https://haproxy.debian.net/bernat.debian.org.gpg |
-        #gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
-    #echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
-        #http://haproxy.debian.net buster-backports-2.4 main \
-        #>/etc/apt/sources.list.d/haproxy.list
-    #sudo apt-get update
-    apt-get -y install haproxy #haproxy=2.4.\*
+    curl https://haproxy.debian.net/bernat.debian.org.gpg |
+        gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+    echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
+        http://haproxy.debian.net buster-backports-2.4 main \
+        >/etc/apt/sources.list.d/haproxy.list
+    sudo apt-get update
+    apt-get -y install haproxy=2.4.\*
 else
     echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
     exit 1
