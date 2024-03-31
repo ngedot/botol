e1f052be7ee2b8beffbbc81e3b806804cf4f8f8a
ngedot <152137599+ngedot@users.noreply.github.com>
2024-03-31 15:06:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -229,13 +229,13 @@ function first_setup(){
     apt -y install haproxy=2.0.\*
 elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
     echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    curl https://haproxy.debian.net/bernat.debian.org.gpg |
-        gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+    curl https://haproxy.debian.net/bernat.debian.org.gpg \
+        | gpg --dearmor > /usr/share/keyrings/haproxy.debian.net.gpg
     echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
-        http://haproxy.debian.net buster-backports-1.8 main \
-        >/etc/apt/sources.list.d/haproxy.list
+        http://haproxy.debian.net buster-backports-2.0 main \
+        > /etc/apt/sources.list.d/haproxy.list
     apt update
-    apt -y install haproxy=1.8.\*
+    apt -y install haproxy=2.0.\*
 else
     echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
     exit 1
