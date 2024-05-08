b5e692bfef048ad4ed3b2ccffef6629d043f8662
ngedot <152137599+ngedot@users.noreply.github.com>
2024-05-08 22:14:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -904,7 +904,7 @@ cat >/etc/cron.d/xp_all <<-END
 		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 		*/20 * * * * root /usr/local/sbin/clearlog
 		END
-    chmod 644 /root/.profile
+#    chmod 644 /root/.profile
 	
     cat >/etc/cron.d/daily_reboot <<-END
 		SHELL=/bin/sh
